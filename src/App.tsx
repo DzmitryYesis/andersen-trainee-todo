2bc174727cc4f0f814bbcdb95e659598123ae666
DzmitryYesis <yesis1988@mail.ru>
2022-08-31 22:13:57 +0300

    refactor: formatting code

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,6 @@ import React, { ReactElement } from 'react';
 
 import './App.css';
 
-const App = (): ReactElement => <div className="App">Todolist test</div>;
+const App = (): ReactElement => <div className="App">Todolist test 2</div>;
 
 export default App;
