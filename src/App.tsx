c43b5d5c222f2ad48fff7e16ae971ec67398a99d
DzmitryYesis <yesis1988@mail.ru>
2022-09-02 14:54:15 +0300

    feat: add possibility get data from server

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 
 import './App.css';
 
-const App = (): ReactElement => <div className="App">Todolist test 2</div>;
+type DataType = {
+  text: string;
+  isFavourite: boolean;
+  isCompleted: boolean;
+  id: string;
+};
+
+const App = (): ReactElement => {
+  const [state, setState] = useState<DataType[]>();
+
+  useEffect(() => {
+    fetch('https://6311e8fc19eb631f9d7b7f47.mockapi.io/todosYesis')
+      .then(res => res.json())
+      .then(data => setState(data));
+  }, []);
+
+  return (
+    <div className="App">
+      Todolist test 2
+      {state?.map(td => (
+        <div key={td.id}>
+          <span>{td.text}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default App;
